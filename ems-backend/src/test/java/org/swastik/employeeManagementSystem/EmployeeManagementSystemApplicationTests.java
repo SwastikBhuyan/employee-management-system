ee749a7c21fc5e65ef47ca1275e997963b84f858
SwastikBhuyan <154882312+SwastikBhuyan@users.noreply.github.com>
2024-07-20 23:48:32 +0530

    Initial commit backend

---
+++ ems-backend/src/test/java/org/swastik/employeeManagementSystem/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.swastik.employeeManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
