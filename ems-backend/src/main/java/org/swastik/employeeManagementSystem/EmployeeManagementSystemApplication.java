af80e5a5b33d0b298ddc90897f20f676a82797a6
SwastikBhuyan <154882312+SwastikBhuyan@users.noreply.github.com>
2024-07-22 00:50:40 +0530

    added cors config

--- ems-backend/src/main/java/org/swastik/employeeManagementSystem/EmployeeManagementSystemApplication.java
+++ ems-backend/src/main/java/org/swastik/employeeManagementSystem/EmployeeManagementSystemApplication.java
@@ -2,6 +2,9 @@ package org.swastik.employeeManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EmployeeManagementSystemApplication {
@@ -10,4 +13,14 @@ public class EmployeeManagementSystemApplication {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
